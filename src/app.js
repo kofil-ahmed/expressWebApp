9311f6a7edc3dd214a71878cfdaff9abd8fe9d88
kofil-ahmed <kofilahmed78846@gmail.com>
2021-02-06 12:19:16 +0600

    3rd commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
-const port = 3500 || process.env.PORT;
+const port = process.env.PORT || 3500;
 
 const app = express();
 
