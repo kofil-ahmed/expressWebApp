68c5e0bc4cd23779e52015c889dd8022ab08a278
kofil-ahmed <kofilahmed78846@gmail.com>
2021-02-07 12:41:33 +0600

    modified all file

--- public/js/script.js
+++ public/js/script.js
@@ -2,6 +2,16 @@ const hidemenu = document.querySelector('.hide_menu');
 const openmenu = document.querySelector('.open_menu');
 const menu = document.querySelector('.head_link_page');
 
+var x = window.matchMedia("(max-width: 800px)")
+function myFunction(x) {
+    if (x.matches) { // If media query matches
+        menu.classList.add('hide');
+    } else {
+        menu.classList.remove('hide');
+    }
+  }
+  myFunction(x) // Call listener function at run time
+  
 hidemenu.addEventListener('click',()=>{
     menu.classList.add('hide');
     openmenu.classList.remove('hide');
